63b963d179c5bd8a9b8798c3655df40d8c1384c2
akmindt <akmindt@gmail.com>
2017-11-12 15:55:34 -0700

    fixes

--- GamersGuild/src/app/app.module.ts
+++ GamersGuild/src/app/app.module.ts
@@ -4,12 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 import { MatButtonModule, MatToolbarModule } from '@angular/material';
-import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
-  {path: 'home', component: HomeCompnent}
+  {path: 'home', component: HomeComponent}
 ]
 
 @NgModule({
