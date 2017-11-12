c5a95a49eda46961188517477c95b2a1d3e2ae94
akmindt <akmindt@gmail.com>
2017-11-12 15:54:17 -0700

    added routing setup

--- GamersGuild/src/app/app.module.ts
+++ GamersGuild/src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 import { MatButtonModule, MatToolbarModule } from '@angular/material';
+
+const appRoutes: Routes = [
+  {path: 'home', component: HomeCompnent}
+]
 
 @NgModule({
   declarations: [
@@ -13,6 +18,10 @@ import { MatButtonModule, MatToolbarModule } from '@angular/material';
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true} // debugging
+    ),
     MatButtonModule,
     MatToolbarModule,
   ],
