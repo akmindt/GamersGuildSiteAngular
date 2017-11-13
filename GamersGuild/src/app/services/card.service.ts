59200809ac36a1aead88860091297822b444084a
alex-shockency <alexshockency@gmail.com>
2017-11-12 22:36:16 -0700

    fixed navbar

--- GamersGuild/src/app/services/card.service.ts
+++ GamersGuild/src/app/services/card.service.ts
@@ -45,5 +45,33 @@ const cards = [
     'formats': ['Vintage'],
     'price': 1252.45,
     'imgUrl': 'http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=600&type=card'
+  },
+  {
+    'name': 'Lightning Bolt',
+    'set': 'Beta',
+    'formats': ['Commander', 'Legacy', 'Modern', 'Vintage'],
+    'price': 150.00,
+    'imgUrl': 'http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=504&type=card'
+  },
+  {
+    'name': 'Lightning Bolt',
+    'set': 'Unlimited',
+    'formats': ['Commander', 'Legacy', 'Modern', 'Vintage'],
+    'price': 3.25,
+    'imgUrl': 'http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=806&type=card'
+  },
+  {
+    'name': 'Black Lotus',
+    'set': 'Unlimited',
+    'formats': ['Vintage'],
+    'price': 1252.45,
+    'imgUrl': 'http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=600&type=card'
+  },
+  {
+    'name': 'Black Lotus',
+    'set': 'Unlimited',
+    'formats': ['Vintage'],
+    'price': 1252.45,
+    'imgUrl': 'http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=600&type=card'
   }
 ]
