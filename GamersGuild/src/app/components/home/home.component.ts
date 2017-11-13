771614da13ce982a5e052659ad6b58ca113752ed
alex-shockency <alexshockency@gmail.com>
2017-11-12 21:43:34 -0700

    Minor UI tweaks

--- GamersGuild/src/app/components/home/home.component.ts
+++ GamersGuild/src/app/components/home/home.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  slideIndex: number;
   constructor() { }
 
   ngOnInit() {
+    this.slideIndex = 1;
+    this.showDivs(this.slideIndex);
+  }
+
+  plusDivs(n) {
+      this.showDivs(this.slideIndex += n);
+  }
+  showDivs(n) {
+      let i;
+      const x = document.getElementsByClassName('mySlides');
+      if (n > x.length) {this.slideIndex = 1; }
+      if (n < 1) {this.slideIndex = x.length; }
+      for (i = 0; i < x.length; i++) {
+          x[i].setAttribute('style', 'display: none;');
+      }
+      x[this.slideIndex - 1].setAttribute('style', 'display: inline-block;');
   }
 
 }
